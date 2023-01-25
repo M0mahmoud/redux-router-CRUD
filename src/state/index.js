10fafab0b6e3a87c73f8c244ac8d087ab520e1d9
M0mahmoud <houdmohamed85@gmail.com>
2023-01-25 21:27:20 +0200

    Upgrading...

--- src/state/index.js
+++
@@ -1,9 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-
-import postSlice from "./postSlice";
-
-const store = configureStore({
-  reducer: { posts: postSlice },
-});
-
-export default store;
